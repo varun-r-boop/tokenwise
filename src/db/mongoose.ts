0448ea5ca872802fd188940966693c68a6712de9
varun-r-boop <varunpanthom6555@gmail.com>
2025-05-05 22:27:34 +0530

    Mongo db context set up and analytics pipeline

--- src/db/mongoose.ts
+++ src/db/mongoose.ts
@@ -1,16 +1,31 @@
-import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import { MongoClient, Db } from 'mongodb';
 
 dotenv.config();
 
 const MONGODB_URI = process.env.MONGODB_URI || '';
+const dbName = process.env.MONGODB_DB || undefined;
+
+let client: MongoClient;
+let db: Db;
 
 export const connectDB = async (): Promise<void> => {
   try {
-    await mongoose.connect(MONGODB_URI);
-    console.log('MongoDB connected successfully');
+    if (!client ) {
+      client = new MongoClient(MONGODB_URI);
+      await client.connect();
+      db = dbName ? client.db(dbName) : client.db();
+      console.log('MongoDB connected');
+    }
   } catch (error) {
     console.error('MongoDB connection error:', error);
     process.exit(1);
   }
 };
+
+export function getDB() {
+  if (!db) {
+    throw new Error('Database not connected. Call connectDB() first.');
+  }
+  return db;
+}
