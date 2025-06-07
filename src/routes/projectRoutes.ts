fee98289edc4d34ccb77fbedb0417aa312f6b1e9
varun-r-boop <varunpanthom6555@gmail.com>
2025-06-07 12:59:20 +0530

    added api to clear project cached responses

--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import projectController from '../controller/projectController.js';
+import { clearCacheByProjectId } from './proxy.js';
 
 const router = express.Router();
 
 router.get('/:projectId', projectController.getProjectDetails);
+router.delete('/:projectId/cache', clearCacheByProjectId);
 
 export default router; 
